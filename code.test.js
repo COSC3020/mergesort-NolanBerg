6b7f36bd085bf033b70b7e42d8412cda7ce362fd
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-21 17:23:03 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,9 +1,9 @@
 const jsc = require('jsverify');
-const { mergeSort } = require('./code.js');
+const { mergesort } = require('./code.js');
 
 const testSort = jsc.forall('array nat', function (arr) {
 
-    mergeSort(arr);
+    mergesort(arr);
 
     const isAscending = arr.every((value, index) => index === 0 || value >= arr[index - 1]);
 
