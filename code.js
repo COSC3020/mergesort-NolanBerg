9383dadd01549e28fe6a0276e7c351d1bb83d914
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-21 17:38:10 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,59 @@
-function mergesort(array) {
-    return array;
+function merge(arr, left, mid, right) {
+  const n1 = mid - left + 1;
+  const n2 = right - mid;
+
+  const L = new Array(n1);
+  const R = new Array(n2);
+
+  for (let i = 0; i < n1; i++) {
+    L[i] = arr[left + i];
+  }
+
+  for (let i = 0; i < n2; i++) {
+    R[i] = arr[mid + 1 + i];
+  }
+
+  let i = 0;
+  let j = 0;
+  let k = left;
+
+  while (i < n1 && j < n2) {
+    if (L[i] <= R[j]) {
+      arr[k] = L[i];
+      i++;
+    } else {
+      arr[k] = R[j];
+      j++;
+    }
+    k++;
+  }
+
+  while (i < n1) {
+    arr[k] = L[i];
+    i++;
+    k++;
+  }
+
+  while (j < n2) {
+    arr[k] = R[j];
+    j++;
+    k++;
+  }
 }
+
+function mergesort(arr) {
+  const n = arr.length;
+  let width = 1;
+  while (width < n) {
+    for (let left = 0; left < n; left += 2 * width) {
+      const mid = Math.min(left + width - 1, n - 1);
+      const right = Math.min(left + 2 * width - 1, n - 1);
+      merge(arr, left, mid, right);
+    }
+    width *= 2;
+  }
+}
+
+module.exports = {
+  mergesort,
+};
