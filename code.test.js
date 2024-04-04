7c48f00c807b31355a2d6b947fc39f8a0d441a38
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-04-04 05:40:08 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,5 +1,5 @@
 const jsc = require('jsverify');
-const { mergesort, merge } = require('./code.js');
+const { mergesort, merge } = require('code.js');
 
 const testSort = jsc.forall('array nat', function (arr) {
 
