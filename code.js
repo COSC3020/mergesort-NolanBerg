2b4bf1e6ff01bf72f15e93db8cbd50ebec58e89f
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-04-04 05:32:49 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,59 +1,37 @@
 function merge(arr, left, mid, right) {
-  const n1 = mid - left + 1;
-  const n2 = right - mid;
 
-  const L = new Array(n1);
-  const R = new Array(n2);
-
-  for (let i = 0; i < n1; i++) {
-    L[i] = arr[left + i];
-  }
-
-  for (let i = 0; i < n2; i++) {
-    R[i] = arr[mid + 1 + i];
-  }
-
-  let i = 0;
-  let j = 0;
-  let k = left;
-
-  while (i < n1 && j < n2) {
-    if (L[i] <= R[j]) {
-      arr[k] = L[i];
-      i++;
-    } else {
-      arr[k] = R[j];
-      j++;
+    let start2 = mid + 1;
+    if (arr[mid] <= arr[start2]) {
+        return;
     }
-    k++;
-  }
-
-  while (i < n1) {
-    arr[k] = L[i];
-    i++;
-    k++;
-  }
-
-  while (j < n2) {
-    arr[k] = R[j];
-    j++;
-    k++;
-  }
+    while (left <= mid && start2 <= right) { 
+        if (arr[left] <= arr[start2]) {
+            left++;
+        } else {
+            let value = arr[start2];
+            let index = start2;
+            while (index !== left) {
+                arr[index] = arr[index - 1];
+                index--;
+            }
+            arr[left] = value;
+            left++;
+            mid++;
+            start2++;
+        }
+    }
 }
 
-function mergesort(arr) {
-  const n = arr.length;
-  let width = 1;
-  while (width < n) {
-    for (let left = 0; left < n; left += 2 * width) {
-      const mid = Math.min(left + width - 1, n - 1);
-      const right = Math.min(left + 2 * width - 1, n - 1);
-      merge(arr, left, mid, right);
+function mergesort(arr, l, r) {
+
+    if (l < r) {
+        let m = l + Math.floor((r - l) / 2);
+        mergesort(arr, l, m);
+        mergesort(arr, m + 1, r);
+        merge(arr, l, m, r);
     }
-    width *= 2;
-  }
 }
 
 module.exports = { mergesort, merge };
- 
+
 
